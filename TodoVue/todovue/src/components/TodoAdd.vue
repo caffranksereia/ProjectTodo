b3cefc96d9e308ebe4e49611a238fbc40b66398b
ferafael <circuncisaofabio@icloud.com>
2021-04-15 09:43:15 -0300

    Componentes TodoAdd Criado

---
+++ TodoVue/todovue/src/components/TodoAdd.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Add</h1>
+    </div>
+
+
+
+</template>
+<script>
+export default {
+    name:'TodoAdd',
+    props:{
+        Titulo:String,
+        Todo:String
+    }
+}
+</script>
+<style scoped>
+
+</style>
