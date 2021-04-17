75b589a37a48d51c144436e98a448d26f605ec7d
ferafael <circuncisaofabio@icloud.com>
2021-04-16 21:07:32 -0300

    Modifica
    Modificar o index router acrescentar done e deletado menu

--- todovue/src/router/index.js
+++ todovue/src/router/index.js
@@ -8,12 +8,18 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/Done',
+    name: 'Done',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "DoneTodo" */ '../views/DoneTodo.vue')
+  },
+ 
+  {
+    path:'/deletados',
+    name:'Deletados',
+    component:() => import('../views/Deletados.vue')
   }
 ]
 
