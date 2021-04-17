50a79ca605a2c8b93d68069d7cfea2fcf3aeb5ab
ferafael <circuncisaofabio@icloud.com>
2021-04-16 21:01:08 -0300

    Component Edit
    Criado o component EditTodo

---
+++ todovue/src/components/EditTodo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="EditTodo">
+      <h1>Editar todo</h1>
+        <form>
+            <label>titulo</label>
+            <input type="text">
+            <label>Todo</label>
+            <input type="text">
+            <label>Done</label>
+            <input type="checkbox" name="" id="">
+            <button @click="Edit()">Update</button>
+            <button>Cancel</button>
+
+
+        </form>
+        
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import TodoApi from '@/Services/TodoApi'
+
+export default {
+  name: 'EditTOdo',
+    props: {
+        titulo:String,
+        todo:String,
+        donneTodo:Boolean
+  },
+  data(){
+
+  },
+  methods:{
+    Edit(){
+      alert('Editar')
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
