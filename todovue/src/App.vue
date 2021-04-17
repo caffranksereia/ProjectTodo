220d91e92f106688330ac2c813a47f9898c53438
ferafael <circuncisaofabio@icloud.com>
2021-04-16 21:09:34 -0300

    Modificar
    acrescentar o components addtodo,deletetodo,ListTodo,
    e apagado o addTodo

--- todovue/src/App.vue
+++ todovue/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/Feito">Done</router-link>|
+    <router-link to="/deletados">Deletados</router-link>|
   </div>
   <router-view/>
 </template>
