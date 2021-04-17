81d13c64156ce661cb1ce7a8e5aeda9563a7e325
ferafael <circuncisaofabio@icloud.com>
2021-04-16 21:00:27 -0300

    Component DelelteTodo
    Criar componente DeleteTodo

---
+++ todovue/src/components/DeleteTodo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="TodoDel">
+        <h1>Delete Todo</h1>
+        <form>
+            <label>titulo</label>
+            <input type="text">
+            <label>Todo</label>
+            <input type="text">
+            <label>Done</label>
+            <input type="checkbox" name="" id="">
+            <button @click="del()">Delete</button>
+            <button>Cancel</button>
+
+
+        </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import TodoApi from '@/Services/TodoApi'
+
+export default {
+  name: 'DeleteTodo',
+  props: {
+    titulo:String,
+    todo:String,
+    donneTodo:Boolean
+  },
+  methods:{
+    del(){
+      alert('Del')
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
